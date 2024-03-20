973f150263597e01460e5d7ee27f66e6d5d192ad
t3rana00 <t3rana00@students.oamk.fi>
2024-03-20 18:55:06 +0200

    Assignment 4 done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+
+
 const BACKEND_ROOT_URL = 'http://localhost:3001'
+import { Todos } from "./class/Todos.js";
+
+const todos = new Todos(BACKEND_ROOT_URL)
 
 const list = document.querySelector('ul')
 const input = document.querySelector('input')
@@ -9,22 +14,21 @@ input.disabled = true
 const renderTask = (task) => {
     const li = document.createElement('li')
     li.setAttribute('class', 'list-group-item')
-    li.innerHTML = task
+    li.innerHTML = task.getText()
     list.append(li)
 }
 
 //create separate function for getting tasks/
-const getTasks = async () => {
-    try {
-        const response = await fetch(BACKEND_ROOT_URL)
-        const json = await response.json()
-        json.forEach(task => {
-            renderTask(task.description)
+const getTasks = () => {
+   
+    todos.getTasks().then((tasks) => {
+        tasks.forEach(task => {
+            renderTask(task)
         })
-        input.disabled = false
-    } catch (error) {
-        alert('Error retrieving tasks' + error.message)
-    }
+        
+    }).catch((error) => {
+        alert(error)
+})
 }
 
 //create separate function for saving task/
@@ -46,21 +50,16 @@ const saveTask = async (task) => {
 
 //key press enter event/
 input.addEventListener('keypress', (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault()
-            const task = input.value.trim()
-            if (task!=='') {
-                const li = document.createElement('li')
-                li.setAttribute('class', 'list-group-item')
-                li.innerHTML = task
-                list.append(li)
-                input.value = ''                        
-            }    
-                saveTask(task).then((json) => {
-                    renderTask(task)
-                    input.value = ''                        
-            })
-        }    
-    });
-    
-getTasks()
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      const task = input.value.trim()
+      if (task !== '') {
+        todos.addTask(task).then((task) => {
+          renderTask(task)
+          input.value = ''   
+          input.focus()                     
+      })
+    }    
+    }
+  });
+  getTasks()
