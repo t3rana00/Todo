5e106fca53e0025d45d375d8891f26da930bd86e
t3rana00 <t3rana00@students.oamk.fi>
2024-03-20 11:04:10 +0200

    Assignemnt 2 done

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = 3001;
+
+app.get('/', (req, res) => {
+    const pool = openDb();
+
+    pool.query('select * from task', (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message })
+        }
+        res.status(200).json(result.rows);
+    })
+    
+});
+
+const openDb = ()=> {
+    const pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'todo',
+        password: 'test',
+        port: 5432,
+    });
+    return pool;
+}
+
+app.listen(port);
+   
+app.post('/new', (req, res) => {
+    const pool = openDb();
+
+    pool.query('insert into task (description) values ($1) returning *', 
+    [req.body.description],
+     (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message })
+        } else {
+        res.status(200).json({ id: result.rows[0].id})
+     }
+    })
+});
