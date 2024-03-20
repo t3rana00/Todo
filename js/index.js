5e6c530fc2012da19b1cdd47cc96a556a7947c9e
t3rana00 <t3rana00@students.oamk.fi>
2024-03-20 14:05:34 +0200

    Assignment 3 done

--- js/index.js
+++ js/index.js
@@ -1,17 +1,66 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
+const BACKEND_ROOT_URL = 'http://localhost:3001'
 
-input.addEventListener('keypress',(event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        const task = input.value.trim();
-        if (task !== '') {
-            const li = document.createElement('li');
-            li.setAttribute('class', 'list-group-item');
-            li.innerHTML = task;
-            list.append(li);
-            input.value = '';
-        }
+const list = document.querySelector('ul')
+const input = document.querySelector('input')
+
+input.disabled = true
+
+//create separate function for rendering task/
+const renderTask = (task) => {
+    const li = document.createElement('li')
+    li.setAttribute('class', 'list-group-item')
+    li.innerHTML = task
+    list.append(li)
+}
+
+//create separate function for getting tasks/
+const getTasks = async () => {
+    try {
+        const response = await fetch(BACKEND_ROOT_URL)
+        const json = await response.json()
+        json.forEach(task => {
+            renderTask(task.description)
+        })
+        input.disabled = false
+    } catch (error) {
+        alert('Error retrieving tasks' + error.message)
     }
-});
+}
 
+//create separate function for saving task/
+const saveTask = async (task) => {
+    try {
+        const json = JSON.stringify({ description: task })
+        const response = await fetch(BACKEND_ROOT_URL + '/new', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: json
+        })
+        return response.json()
+    } catch (error) {
+        alert('Error saving task' + error.message)
+    }
+}
+
+//key press enter event/
+input.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault()
+            const task = input.value.trim()
+            if (task!=='') {
+                const li = document.createElement('li')
+                li.setAttribute('class', 'list-group-item')
+                li.innerHTML = task
+                list.append(li)
+                input.value = ''                        
+            }    
+                saveTask(task).then((json) => {
+                    renderTask(task)
+                    input.value = ''                        
+            })
+        }    
+    });
+    
+getTasks()
